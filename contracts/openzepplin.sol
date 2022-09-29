5be229e4360285bd173be85a0adcf1a4aa3b0fcc
Azutech <azunna.onugha@gmail.com>
2022-09-29 13:39:36 +0100

    the contracts are set

--- contracts/openzepplin.sol
+++ contracts/openzepplin.sol
@@ -6,7 +6,7 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 
-contract openzeppelin is ERC721, Ownable {
+contract Openzeppelin is ERC721, Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
@@ -14,44 +14,43 @@ contract openzeppelin is ERC721, Ownable {
     bool public isPublicMintEnabled;
     string internal baseTokenUri;
     address payable public withdrawWallet;
-
     mapping(address => uint256) public walletsMints;
 
-    constructor() payable ERC721('openzeppelin', 'OP'){
+    constructor() payable ERC721('Openzeppelin', 'OP'){
         mintPrice = 0.02 ether;
         totalSupply = 0;
         maxSupply  = 1000;
         maxPerWallet = 3;
     }
 
-    function isPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
+    function SetisPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
         isPublicMintEnabled = isPublicMintEnabled_;
     }
 
     function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
-        baseTokenUri = baseTokenUri_
+        baseTokenUri = baseTokenUri_;
     }
 
-    function tokenURI(unit256 tokenId_) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId_) public view override returns (string memory) {
         require(_exists(tokenId_), 'Token does not exist!');
-        return string(abi.encodePacked(baseTokenUri, String.toString(tokenId_), '.json'))
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), '.json'));
     }
 
     function withdraw() external onlyOwner {
-        (bool success, ) =withdrawWallet.call{value: address(this).balance}('')
-        require (success, 'withdraw fai led')
+        (bool success, ) =withdrawWallet.call{value: address(this).balance}('');
+        require (success, 'withdraw fai led');
     }
 
     function mint(uint256 quantity_) public payable {
-        require(isPublicMintEnabled, 'minting not enabled')
-        require(msg.value == quantity_ * mintPrice, 'wrong mint ')
-        require(totalSupply + quantity_ <= maxSupply, 'sold out')
-        require(walletsMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet')
+        require(isPublicMintEnabled, 'minting not enabled');
+        require(msg.value == quantity_ * mintPrice, 'wrong mint ');
+        require(totalSupply + quantity_ <= maxSupply, 'sold out');
+        require(walletsMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
 
         for (uint256 i = 0; i < quantity_; i++){
             uint256 newTokenId = totalSupply + 1;
             totalSupply++;
-            _safeMint(msg.sender, newTokenId)
+            _safeMint(msg.sender, newTokenId);
         }
     }
 }

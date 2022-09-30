c543b6611a47673e3fa321fedda4be0a86d08df4
Azutech <azunna.onugha@gmail.com>
2022-09-30 15:16:10 +0100

    bg-fixed contracts has been deployed bugs has been fixed

---
+++ scripts/deployOpenzeppelin.ts
@@ -0,0 +1,16 @@
+import hre from 'hardhat'
+
+const main = async () =>{
+    const OpenzeppelinNFT= await hre.ethers.getContractFactory('Openzeppelin');
+    const openzeppelinNFT = await OpenzeppelinNFT.deploy()
+    
+    await openzeppelinNFT.deployed();
+
+    console.log('OpenzepplinNFT is deployed:', openzeppelinNFT.address)
+}
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
